8edfd6326dd73d17bcec4565db9d7ff453fb32fc
Ping Zhao <pzhao11@gmail.com>
2020-03-09 19:25:56 -0700

    Initial Commit

---
+++ 2020-03-9-ClassExercise-Ping-Zhao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-9-ClassExercise-Ping-Zhao
+//
+//  Created by Ping Zhao on 3/9/20.
+//  Copyright © 2020 Ping Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
